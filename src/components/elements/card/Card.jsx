7646b698a2d082dda606e185c6bb9101c5bb8dc1
puspa <blackie002@gmail.com>
2021-01-02 12:03:51 -0500

    card component setup complate

--- src/components/elements/card/Card.jsx
+++ src/components/elements/card/Card.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./card.scss";
+import "./progress.scss";
 import cardImage from "../../../images/all_image.jpg";
 
 const Card = () => {
   return (
     <div className="card_main">
       <img src={cardImage} alt="data-image" className="card_image" />
-      <div className="user_rating"></div>
-      <h2>Title of the Movie</h2>
-      <p>Release Date</p>
+      <div className="card_contain">
+          {/* pass data value to the className Aswell */}
+        <div className="progress--circle progress--40">
+          <div className="progress__number">86<sup style={{fontSize:".6rem"}}>%</sup></div>
+        </div>
+        <h2>Title of the Movie</h2>
+        <p>Release Date</p>
+      </div>
     </div>
   );
 };
